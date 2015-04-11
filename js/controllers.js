da907edb0c2b18bc072fd12845eb30f87ff26b37
Mai <mxngyn@gmail.com>
2015-04-11 14:43:23 -0400

    Indentation

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,13 @@
 /* LANDING PAGE CONTROLLER */
 
 app.controller('AppCtrl', ['$scope', 'movies', 'SearchQuery', function($scope, movies, SearchQuery) {
-  $scope.name = "Reel Movies"
   movies.success(function(data) {
     console.log(data);
     $scope.movies = data;
   });
   $scope.query = '';
   $scope.$watch('query', function (newValue, oldValue) {
-      if (newValue !== oldValue) SearchQuery.setSearchQuery(newValue);
+    if (newValue !== oldValue) SearchQuery.setSearchQuery(newValue);
   });
   $scope.logo = 'imgs/reelmovies_big.png';
   $scope.search = 'imgs/search.png'
@@ -18,14 +17,13 @@ app.controller('AppCtrl', ['$scope', 'movies', 'SearchQuery', function($scope, m
 /* RESULTS PAGE CONTROLLER */
 
 app.controller('ResultsCtrl', ['$scope', 'movies', 'SearchQuery', function($scope, $filter, movies, SearchQuery) {
-  $scope.name = "Reel Movies"
   movies.success(function(data) {
     console.log(data);
     $scope.movies = data;
   });
   $scope.$watch(function () { return SearchQuery.getSearchQuery(); }, function (newValue, oldValue) {
-          if (newValue !== oldValue) $scope.query = newValue;
-      });
+    if (newValue !== oldValue) $scope.query = newValue;
+  });
   $scope.queryData = $filter('filter')(names, query);
 
 }]);
