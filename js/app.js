296e55f35ae9be3affde5e577cf817954fb5f560
Mai <mxngyn@gmail.com>
2015-04-10 20:27:26 -0400

    App routes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-var app = angular.module("reelMovies", []);
+var app = angular.module("ReelMovies", ['ngRoute']);
+
+
+app.config(["$routeProvider", function ($routeProvider) {
+  $routeProvider
+    .when("/", {controller: "AppCtrl", templateUrl: "views/home.html"})
+    .otherwise({redirectTo: "/"})
+}])
