d207d41a77780d718d099f71dc30024ce2165f7c
Mai <mxngyn@gmail.com>
2015-04-10 20:51:38 -0400

    Add search query

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,5 @@
 app.controller('AppCtrl', ['$scope', 'movies', function($scope, movies) {
+  $scope.name = "Reel Movies"
   movies.success(function(data) {
     console.log(data);
     $scope.movies = data;
