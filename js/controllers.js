30d113085c173e1bd5c150a76f8d076b1ea896f4
Mai <mxngyn@gmail.com>
2015-04-11 03:26:39 -0400

    Controllers

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,25 @@
-app.controller('AppCtrl', ['$scope', 'movies', function($scope, movies) {
+app.controller('AppCtrl', ['$scope', 'movies', 'SearchQuery', function($scope, movies) {
   $scope.name = "Reel Movies"
   movies.success(function(data) {
     console.log(data);
     $scope.movies = data;
   });
+  $scope.query = '';
+  $scope.$watch('query', function (newValue, oldValue) {
+      if (newValue !== oldValue) SearchQuery.setSearchQuery(newValue);
+  });
+  $scope.logo = '../images/reelmovies_big.png'
+}]);
+
+app.controller('ResultsCtrl', ['$scope', 'movies', 'SearchQuery', function($scope, $filter, movies) {
+  $scope.name = "Reel Movies"
+  movies.success(function(data) {
+    console.log(data);
+    $scope.movies = data;
+  });
+  $scope.$watch(function () { return SearchQuery.getSearchQuery(); }, function (newValue, oldValue) {
+          if (newValue !== oldValue) $scope.query = newValue;
+      });
+  $scope.queryData = $filter('filter')(names, query));
 
 }]);
