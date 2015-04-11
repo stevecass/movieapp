db76116f4ccb5d4ccb601b9f79eba9a0ebd8a563
Mai <mxngyn@gmail.com>
2015-04-10 20:44:31 -0400

    Move services folder

---
+++ js/services/movies.js
@@ -0,0 +1,9 @@
+app.factory('movies', ['$http', function($http) {
+  return $http.get('http://mxngyn.com/movieapp/movies.json')
+  .success(function(data) {
+    return data;
+  })
+  .error(function(err) {
+    return err;
+  });
+}]);
--- services/movies.js
+++
@@ -1,9 +0,0 @@
-app.factory('movies', ['$http', function($http) {
-  return $http.get('http://mxngyn.com/movieapp/movies.json')
-  .success(function(data) {
-    return data;
-  })
-  .error(function(err) {
-    return err;
-  });
-}]);
