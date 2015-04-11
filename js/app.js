9396f697af2ae34b98ba7b0238158cd91e6d9c35
Mai <mxngyn@gmail.com>
2015-04-11 03:26:29 -0400

    Route for results

--- js/app.js
+++ js/app.js
@@ -4,5 +4,6 @@ var app = angular.module("ReelMovies", ['ngRoute']);
 app.config(["$routeProvider", function ($routeProvider) {
   $routeProvider
     .when("/", {controller: "AppCtrl", templateUrl: "views/home.html"})
+    .when("/results", {controller: "ResultsCtrl", templateUrl: "views/results.html"})
     .otherwise({redirectTo: "/"})
 }])
