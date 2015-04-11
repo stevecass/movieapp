bb8b633cc440c8ad27f43409387781720e3900b8
Mai <mxngyn@gmail.com>
2015-04-11 03:26:53 -0400

    Service for getting data from query

--- js/services/movies.js
+++ js/services/movies.js
@@ -1,3 +1,15 @@
+app.factory('SearchQuery', function(){
+  var data = { MovieQuery: "" };
+  return {
+    getSearchQuery: function () {
+      return data.MovieQuery;
+    },
+    setSearchQuery: function (query) {
+      data.MovieQuery = query;
+    }
+  };
+});
+
 app.factory('movies', ['$http', function($http) {
   return $http.get('http://mxngyn.com/movieapp/movies.json')
   .success(function(data) {
