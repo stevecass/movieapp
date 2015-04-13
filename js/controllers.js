b916660d455b58a2a396b70a413dd363af14f50e
Mai <mxngyn@gmail.com>
2015-04-12 23:01:03 -0400

    Change jQuery selector for percentages

--- js/controllers.js
+++ js/controllers.js
@@ -35,8 +35,8 @@ app.controller('ResultsCtrl', ['$scope', 'GetMovieJson', 'SearchQuery', function
     $scope.movies = data;
   });
   $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
-    $(".percentage").each(function(){
-      var percentageNum = parseInt(this.closest('.gray-circle').children());
+    $(".gray-circle").each(function(){
+      var percentageNum = parseInt(this.children('span').innerHTML);
       var moveFromTop = 100 - percentageNum;
       if(percentageNum > 80) {
         $(this).css({"top": moveFromTop + "%", "background-color": "#86c60c"});
