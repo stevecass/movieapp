317b95bb09d677eb2c4f742d27c06047694f5bc3
Mai <mxngyn@gmail.com>
2015-04-13 15:59:18 -0400

    Change get to jsonp

--- js/services/movies.js
+++ js/services/movies.js
@@ -1,5 +1,5 @@
 app.factory('GetMovieJson',['$http', function($http) {
-  return $http.get('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=j2efeebhdf9fd92447c7kcsr').success(function(data) {
+  return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0.json?apikey=j2efeebhdf9fd92447c7kcsr&callback=JSON_CALLBACK').success(function(data) {
   return data;
   }).error(function(err) {
     return err;
