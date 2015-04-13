11d690ed072e85a9286fe50220db33cfbed3f6f5
Mai <mxngyn@gmail.com>
2015-04-12 22:30:27 -0400

    Add conditional for percentage colors

--- js/controllers.js
+++ js/controllers.js
@@ -13,7 +13,7 @@ app.controller('AppCtrl', ['$scope', 'SearchQuery', function($scope, SearchQuery
 
 app.controller('SearchCtrl', ['$scope', '$location', 'SearchQuery', function($scope, $location, SearchQuery) {
   $scope.changeView = function(view){
-      $location.path(view);
+    $location.path(view);
   }
   $scope.updateQuery = function() {
     SearchQuery.setData($scope.query)
@@ -24,31 +24,31 @@ app.controller('SearchCtrl', ['$scope', '$location', 'SearchQuery', function($sc
 /* RESULTS PAGE CONTROLLER */
 
 app.controller('ResultsCtrl', ['$scope', 'GetMovieJson', 'SearchQuery', function($scope, GetMovieJson, SearchQuery) {
-    $scope.updateQuery = function() {
-      SearchQuery.setData($scope.query)
-    };
-    $scope.getQuery = function() {
-      $scope.factoryData = SearchQuery.getData();
-    }
+  $scope.updateQuery = function() {
+    SearchQuery.setData($scope.query)
+  };
+  $scope.getQuery = function() {
     $scope.factoryData = SearchQuery.getData();
-    GetMovieJson.success(function(data) {
-      $scope.movies = data;
-    });
-    $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
-      if($(".percentage")){
-        $(".percentage").each(function(){
-          var percentageNum = parseInt(this.children[0].innerHTML);
-          var moveFromTop = 100 - percentageNum;
-          $(this).css("top", moveFromTop + "%" );
-        });
+  }
+  $scope.factoryData = SearchQuery.getData();
+  GetMovieJson.success(function(data) {
+    $scope.movies = data;
+  });
+  $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
+    $(".percentage").each(function(){
+      var percentageNum = parseInt(this.children[0].innerHTML);
+      var moveFromTop = 100 - percentageNum;
+      if(moveFromTop < 10){
+      $(this).css({"top": moveFromTop + "%", "background-color": "#86c60c"});
       }
     });
+  });
 }]);
 
 /* MOVIE SHOW CONTROLLER */
 
 app.controller('MovieCtrl', ['$scope', 'GetMovieJson', '$routeParams', function($scope, GetMovieJson, $routeParams) {
-    GetMovieJson.success(function(data) {
-      $scope.detail = data.movies[$routeParams.id];
-    });
+  GetMovieJson.success(function(data) {
+    $scope.detail = data.movies[$routeParams.id];
+  });
 }]);
