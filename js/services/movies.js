ef10f9d7209e2558d19de5dab3941b5cf2ea4562
Mai <mxngyn@gmail.com>
2015-04-13 15:51:33 -0400

    Edit json url

--- js/services/movies.js
+++ js/services/movies.js
@@ -1,5 +1,5 @@
 app.factory('GetMovieJson',['$http', function($http) {
-  return $http.get('http://mxngyn.com/movieapp/movies.json').success(function(data) {
+  return $http.get('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=j2efeebhdf9fd92447c7kcsr').success(function(data) {
   return data;
   }).error(function(err) {
     return err;
