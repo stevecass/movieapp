d672a880beef9bddb05a75bc8f26f071afcce558
Mai <mxngyn@gmail.com>
2015-04-13 16:02:29 -0400

    Change the return of GetMovieJson factory

--- js/services/movies.js
+++ js/services/movies.js
@@ -1,6 +1,6 @@
 app.factory('GetMovieJson',['$http', function($http) {
   return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0.json?apikey=j2efeebhdf9fd92447c7kcsr&callback=JSON_CALLBACK').success(function(data) {
-  return data;
+  return data.links.movies;
   }).error(function(err) {
     return err;
     });
